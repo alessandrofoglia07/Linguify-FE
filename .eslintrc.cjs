2b3902a3f2e41ff2a5b9d2b8836107e3ee4a0d09
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-07-01 19:44:47 +0200

    ChooseLang component finished

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     rules: {
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
         'no-restricted-imports': [
             'error',
             {
--- src/components/ChooseLang.tsx
+++ src/components/ChooseLang.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useState, useEffect } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
 import { Language } from '@/types';
 
@@ -10,9 +10,48 @@ interface IProps {
 }
 
 const ChooseLang: React.FC<IProps> = ({ num, lang, setLang, possibleLangs }: IProps) => {
+    const [langToChoose, setLangToChoose] = useState<Language[]>(possibleLangs);
+    const [input, setInput] = useState<string>(lang.name);
+
+    useEffect(() => {
+        if (possibleLangs !== langToChoose) setLangToChoose(possibleLangs);
+        if (num === 1) setLangToChoose([{ name: 'Detect Language', code: '/' }, ...possibleLangs]);
+    }, [possibleLangs]);
+
+    useEffect(() => {
+        if (lang.name !== input) setInput(lang.name);
+    }, [lang]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const value = e.target.value;
+
+        setInput(value);
+        setLangToChoose(possibleLangs.filter((lang: Language) => lang.name.toLowerCase().slice(0, value.length) === value.toLowerCase()));
+
+        const foundLang = langToChoose.find((lang: Language) => lang.name.toLowerCase() === value.toLowerCase());
+        if (foundLang) {
+            setLang(foundLang);
+            setInput(foundLang.name);
+        }
+    };
+
+    const handleAutocompleteChange = (e: React.ChangeEvent<object>, value: string | null): void => {
+        if (value) {
+            setLang(langToChoose.find((lang: Language) => lang.name.toLowerCase() === value.toLowerCase()) || possibleLangs[0]);
+            setInput(value);
+        }
+    };
+
     return (
         <div className='chooseLang'>
-            <Autocomplete className='setLangBtn' id={`langSelector${num}`} options={[]} readOnly renderInput={(params) => <TextField {...params} placeholder={lang.name} />} />
+            <Autocomplete
+                className='setLangBtn'
+                id={`langSelector${num}`}
+                options={langToChoose.map((lang: Language) => lang.name)}
+                onChange={handleAutocompleteChange}
+                value={input}
+                renderInput={(params) => <TextField {...params} onChange={handleInputChange} placeholder='Select a language' />}
+            />
         </div>
     );
 };
